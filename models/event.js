8f2b575e3b274b9e6d32a6f4f2d388c6a650a1de
tnsherpa1 <tnsherpa1@gmail.com>
2016-03-27 12:29:26 -0700

    adds user schema and event schema

---
+++ models/event.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var eventSchema = new mongoose.Schema({
+  interest: string,
+  
+
+});
---
+++ models/user.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.schema({
+
+});
