00d361006190bbd9596655bbd1ce02a6f569722d
tnsherpa1 <tnsherpa1@gmail.com>
2016-03-27 12:38:54 -0700

    defines and exports user schema and event schema

--- models/event.js
+++ models/event.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 
 var eventSchema = new mongoose.Schema({
   interest: string,
-  
+
 
 });
+
+var Event = mongoose.model('Event', eventSchema);
+module.exports = Event;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.schema({
+  
 
 });
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
