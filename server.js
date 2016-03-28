d45e91f71286737f0b6abff093c5d8bfe4e28a5d
tnsherpa1 <tnsherpa1@gmail.com>
2016-03-27 17:11:02 -0700

    adds put and delete

--- server.js
+++ server.js
@@ -55,8 +55,35 @@ app.get('api/events/:id', function(req, res){
 })
 
 //UPDATE EVENTS
+app.put('api/events/:id', function(req,res){
+	var id = req.params.id;
+	Event.findById({_id: id}, function(err, updateEvent){
+		if (err) {
+			res.status(500).json(err);
+		} else {
+			updateEvent.title = req.body.title;
+			Event.save(function(err, savedEvent){
+				if(err){
+					res.status(500).json({error: err.message})
+				} else {
+					res.json(savedEvent);
+				}
+			});
+		}
+	})
+});
 
 //DELETE EVENTS
+app.delete('api/events/:id', function(req,res){
+	var id = req.params.id;
+	Event.findOneandRemove({_id: id}, function(err, removedEvent){
+		if (err){
+			res.status(500).json({error: err.message});
+		} else {
+			res.json(removedEvent)
+		}
+	})
+})
 
 
 app.get('*', function(req, res){
